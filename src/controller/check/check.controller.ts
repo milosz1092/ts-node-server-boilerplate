0751202851ca4699d3f85710e76a24f8531afdd0
Milosz Szewczyk <milosz.szewczyk@avid.com>
2019-11-04 14:05:15 +0100

    Add parameters handling

--- src/controller/check/check.controller.ts
+++ src/controller/check/check.controller.ts
@@ -2,9 +2,9 @@ import * as express from 'express';
 
 const router = express.Router({mergeParams: true});
 
-router.route('/getRequest')
+router.route('/getRequest/:id')
 .get((req, res) => {
-    res.json({ success: true });
+    res.json({ success: true, id: req.params.id });
 });
 
 export default router;
